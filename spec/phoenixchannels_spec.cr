1a521df6cdb0664688fba0a2502874afce878db4
bit4bit <bit4bit@noemail.net>
2022-01-18 02:30:26 +0000

    add phoenix heartbeat
    FossilOrigin-Name: 4f795cc52574fa35028439da771540bdfd9d8e93a8bc3ed60eccbb8a5fdba90f

--- spec/phoenixchannels_spec.cr
+++ spec/phoenixchannels_spec.cr
@@ -1,9 +1,26 @@
 require "./spec_helper"
 
 describe Phoenixchannels do
-  # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  describe Phoenixchannels::Serializer do
+    example_msg = Phoenixchannels::Message(Hash(String,Int32)).new(
+      join_ref: "0",
+      ref: "1",
+      topic: "t",
+      event: "e",
+      payload: {"foo" => 1}
+    )
+    
+    it "encodes" do
+      Phoenixchannels::Serializer.encode(example_msg).should eq("[\"0\",\"1\",\"t\",\"e\",{\"foo\":1}]")
+    end
+
+    it "decodes"do
+      msg = Phoenixchannels::Serializer.decode("[\"0\",\"1\",\"t\",\"e\",{\"foo\":1}]") do |payload_as_json|
+        Hash(String,Int32).from_json(payload_as_json)
+      end
+        
+      msg.should eq(example_msg)
+    end
   end
 end
--- src/phoenixchannels.cr
+++ src/phoenixchannels.cr
@@ -1,46 +1,176 @@
 require "http/web_socket"
 require "uri"
 require "socket"
+require "json"
+require "log"
 
 module Phoenixchannels
   VERSION = "0.1.0"
   DEFAULT_VSN = "2.0.0"
 
-  class Socket
-    @ws : HTTP::WebSocket?
+  Log = ::Log.for("phoenix-channels")
+  
+  alias MessageJoinRef = String | Nil
+  alias MessageRef = String
+  alias MessageTopic = String
+  alias MessageEvent = String
+  alias MessagePayload = Hash(String, String | Int32 ) | Hash(String, Int32) | Hash(String, String)
 
+  class Message(T)
+    getter :join_ref
+    getter :ref
+    getter :topic
+    getter :event
+    getter :payload
+
+    def initialize(@join_ref : MessageJoinRef?, @ref : MessageRef, @topic : MessageTopic, @event : MessageEvent, @payload : T)
+    end
+
+    def hash
+      {join_ref: @join_ref,
+       ref: @ref,
+       topic: @topic,
+       event: @event,
+       payload: @payload}.hash
+    end
+
+    def ==(other)
+      other.hash == hash
+    end
+  end
+
+  class Serializer
+
+    def self.encode(msg : Message)
+      [
+        msg.join_ref,
+        msg.ref,
+        msg.topic,
+        msg.event,
+        msg.payload
+      ].to_json
+    end
+
+    def self.decode(data : String, &decoder_payload : String -> Payload) : Message(Payload) forall Payload
+
+      result = JSON.parse(data)
+      join_ref = nil
+      if result[0].as_s?
+        join_ref = result[0].as_s
+      end
+
+      Message(Payload).new(
+        join_ref: join_ref,
+        ref: result[1].as_s,
+        topic: result[2].as_s,
+        event: result[3].as_s,
+        payload: decoder_payload.call(result[4].to_json))
+    end
+  end
+
+  class Socket
+    @ws : HTTP::WebSocket
+    @serializer = Serializer
+    @heartbeat_timeout = 1
+    @on_messages = Array(Proc(String, Nil)).new
+    @ref = 1
+    
     class Error < Exception
     end
 
     def initialize(address : String)
       # socket.js#144
-      @uri = URI.parse("#{address}/websocket?vsn=#{DEFAULT_VSN}")
-    end
-
-    def connect
-      ws = HTTP::WebSocket.new(@uri)
+      uri = URI.parse("#{address}/websocket?vsn=#{DEFAULT_VSN}")
+      @ws = HTTP::WebSocket.new(uri)
 
       spawn do
-        ws.run
+        @ws.run
       end
 
-      @ws = ws
-    rescue ex : ::Socket::Error
-      raise Error.new(ex.message)
+      install_heartbeat()
+
+      @ws.on_message do |raw|
+        Log.debug { "websocket message: #{raw}" }
+
+        @on_messages.each do |on_message|
+          on_message.call(raw)
+        end
+      end
+    end
+
+    def run
+
     end
 
     def abnormalClose(reason : String)
       # socket.js#486
-      conn.close(HTTP::WebSocket::CloseCode::NormalClosure, reason)
+      @ws.close(HTTP::WebSocket::CloseCode::NormalClosure, reason)
     end
 
-    private def conn : HTTP::WebSocket
-      ws = @ws
-      if !ws.nil?
-        ws
-      else
-        raise Error.new("invalid connection")
+    private def send_heartbeat()
+      return if @ws.closed?
+
+      ref = make_ref()
+
+      send(Message.new(topic: "phoenix",
+                       event: "heartbeat",
+                       payload: {} of String => String,
+                                      join_ref: nil,
+                                      ref: ref))
+
+      ref
+    end
+
+    private def send(msg : Message)
+      @ws.send(@serializer.encode(msg))
+    rescue ex : Socket::Error
+      raise Error.new(ex.message)
+    end
+
+    private def make_ref()
+      @ref += 1
+      @ref.to_s
+    end
+
+    private def install_heartbeat()
+      spawn do
+        ch = stream_messages do |payload|
+          payload
+        end
+
+        ref = send_heartbeat()
+
+        loop do
+          select
+          when msg = ch.receive
+            if msg.ref == ref
+              Log.debug { "sending heartbeat" }
+              ref = send_heartbeat()
+            end
+          when timeout(@heartbeat_timeout.seconds)
+            ref = send_heartbeat()
+          end
+        end
       end
+    end
+
+    private def attach_on_messages(&block : String ->)
+      @on_messages << block
+    end
+
+    def stream_messages(&decoder_payload : String -> Payload) : Channel(Message(Payload)) forall Payload
+      ch = Channel(Message(Payload)).new(1)
+
+      attach_on_messages do |raw|
+        if @ws.closed?
+          next
+        end
+
+        msg = @serializer.decode(raw, &decoder_payload)
+        ch.send msg
+      end
+
+      return ch
     end
   end
 end
