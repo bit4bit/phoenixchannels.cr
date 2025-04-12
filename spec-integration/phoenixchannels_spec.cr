9418802ae090348d487f0705354e7fc4dc607992
bit4bit <bi4bit@riseup.net>
2025-04-12 11:48:04 +0000

    improv
    FossilOrigin-Name: 51664f7e934c4f320b586e798f81328771cb0d675ad83aef69bd55231f16d7ba

--- spec-integration/phoenixchannels_spec.cr
+++ spec-integration/phoenixchannels_spec.cr
@@ -4,22 +4,9 @@ require "log"
 Log.setup_from_env(default_level: :debug)
 
 describe Phoenixchannels do
-  it "connect" do
-    socket = Phoenixchannels::Socket.new("ws://elixir-spec.dev:4000/socket")
-    stream = socket.stream_messages(Hash(String, JSON::Any))
-
-    select
-    when msg = stream.receive
-      msg.topic.should eq("phoenix")
-    when timeout(3.seconds)
-      fail "expected messaget with topic phoenix"
-    end
-    socket.abnormal_close("test")
-  end
-
   it "connect channel not raises exception" do
     socket = Phoenixchannels::Socket.new("ws://elixir-spec.dev:4000/socket")
-    channel = socket.channel("echo:lobby", "super payload")
+    channel = socket.channel("echo:lobby", {"demo" => "super payload"})
 
     stream = channel.stream_messages(Hash(String, JSON::Any?))
     select
